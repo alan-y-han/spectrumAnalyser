d817ba8300d0aa51586c5920de7b99723ce7154a
Alan Han <alan.y.han@gmail.com>
2018-06-25 19:52:12 +0100

    Visualise amplitudes with exponential func

--- spectrumAnalyser.py
+++ spectrumAnalyser.py
@@ -20,8 +20,8 @@ def compute_fft(data, bins, weighting):
     for i in xrange(len(matrix)):
         matrix[i] = int(np.mean(power[bins[i] : bins[i + 1]]))
 
-    matrix = np.divide(np.multiply(matrix, weighting), 1500000)
-    matrix = matrix.clip(0, 7)
+    matrix = np.divide(np.multiply(matrix, weighting), 1000000)
+    # matrix = matrix.clip(0, 7)
 
     return matrix
 
@@ -29,15 +29,28 @@ def compute_fft(data, bins, weighting):
 MDP_HEIGHT = 6
 
 def drawMatrix(matrix):
+
+    gamma = 1.5
+
     mdp.clear()
+
     for x in range(len(matrix)):
-        for y in range(int(matrix[x])):
-            buf_x = x + ((x / 5) * 3)
-            mdp.set_pixel(buf_x, MDP_HEIGHT - y, 1)
+        buf_x = x + ((x / 5) * 3)
+
+        for y in xrange(7):            
+            if matrix[x] > pow(gamma, y):
+                mdp.set_pixel(buf_x, MDP_HEIGHT - y, 1)
+            else:
+                break
+
+
+        # for y in range(int(matrix[x])):
+            
+
     mdp.show()
 
 
-def sigIntHandler(signal, frame):
+def sigIntHandler(signalNo, frame):
     # clean up
     inStream.stop_stream()
     inStream.close()
@@ -80,7 +93,7 @@ if __name__ == '__main__':
     print bins
 
     # weighting
-    maxWeight = 48
+    maxWeight = 32
     scaleF = float(maxWeight - 1) / pow(gamma, noBins - 1)
     weighting = (np.array([scaleF * pow(gamma, x) + 1 for x in xrange(noBins)]))
     print weighting
